45c5c9211dcfd56d12d06d6d6cffd13d1babbb07
modzel132 <modzel132@gmail.com>
2018-11-12 22:27:49 +0100

    Dodanie plików

---
+++ lokalizacja.py
@@ -0,0 +1,12 @@
+import requests
+Key ="0d1356a0c37774b2f1b132913381d9b1"
+def funkcja_szer_dlug(szerokosc):
+    lista=[]
+    url = "https://developers.zomato.com/api/v2.1/search?lat="+str(szerokosc)+"&lon=18&radius=2&sort=real_distance"
+    r = requests.get(url, headers={'user-key': Key})
+    data =r.json()
+    miejsca = data['restaurants']
+    for miejsce in miejsca:
+        lista.append(str(miejsce['restaurant']['location']['city']))
+    return str(lista)
+
---
+++ polaczenie.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+from lokalizacja import funkcja_szer_dlug
+
+app = Flask(__name__,template_folder='Strona')
+
+@app.route('/wynik',methods=['POST'])
+def wynik():
+    szerokosc = request.form['szerokosc']
+    return funkcja_szer_dlug(szerokosc)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
